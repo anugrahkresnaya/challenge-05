121654fec9132caae3765b9f090cc219159ed471
anugrahkresnaya <anugrahkresnaya@gmail.com>
2022-10-07 00:45:29 +0700

    refactor: add many things to add and delete cars data

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = 8000;
 
 const path = require('path');
+const { response } = require('express');
 const PUBLIC_DIRECTORY = path.join(__dirname, 'public');
 
 app.use(express.static(PUBLIC_DIRECTORY));
+app.use(express.json());
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  const url = 'http://localhost:8002/'
+  axios.get(url)
+  .then(response => {
+    res.render('index', { cars: response.data });;
+  })
+  .catch(err => {
+    console.log(err)
+  })
 });
+
+app.get('/deleteCar/:id', (req, res) => {
+  const carId = req.params.id;
+  axios.delete(`http://localhost:8002/cars/${carId}`).then(response => {
+    res.redirect('/')
+  })
+})
+
+app.get('/updateCar/:id', (req, res) => {
+  res.render('updateCar', { data: req.params.id })
+})
 
 app.get('/addCar', (req, res) => {
   res.render('addCar');
