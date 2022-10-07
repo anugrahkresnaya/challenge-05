0207d525866418535e30248b769ba719ac7256c4
anugrahkresnaya <anugrahkresnaya@gmail.com>
2022-10-07 10:50:07 +0700

    refactor: add multer and cloudinary to upload and update file

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const axios = require('axios');
 const app = express();
 const port = 8000;
 
+const upload = require('./upload');
+const cloudinary = require('./cloudinary');
+
 const path = require('path');
-const { response } = require('express');
 const PUBLIC_DIRECTORY = path.join(__dirname, 'public');
 
 app.use(express.static(PUBLIC_DIRECTORY));
@@ -22,16 +24,49 @@ app.get('/', (req, res) => {
   })
 });
 
-app.get('/deleteCar/:id', (req, res) => {
+app.get('/deleteCar/:id', (req, res, next) => {
   const carId = req.params.id;
   axios.delete(`http://localhost:8002/cars/${carId}`).then(response => {
-    res.redirect('/')
+    res.redirect('/', {  });
   })
-})
+});
 
-app.get('/updateCar/:id', (req, res) => {
-  res.render('updateCar', { data: req.params.id })
-})
+app.get('/updateCar/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const cars = await axios.get(`http://localhost:8002/cars/${id}`);
+    res.render('updateCar', cars.data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+app.post('/updateCar/:id', upload.single('image'), (req, res) => {
+  const fileBase64 = req.file.buffer.toString("base64");
+  const file = `data:${req.file.mimetype};base64,${fileBase64}`;
+
+  cloudinary.uploader.upload(file, { folder: 'uploads' },async function(err, result) {
+    if (!!err) {
+      console.log(err);
+      return res.status(400).json({
+        message: "gagal upload"
+      })
+    }
+
+    const id = req.params.id;
+    const body = req.body;
+    body.image = result.url;
+    try {
+      const cars = await axios.put(`http://localhost:8002/cars/${id}`, body);
+      return res.redirect('/');
+    } catch (err) {
+      return res.status(500).json({
+        message: "gagal",
+        err: err
+      })
+    }
+  })
+});
 
 app.get('/addCar', (req, res) => {
   res.render('addCar');
